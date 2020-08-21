41953aace53f310c041b4cd74006ec4ed19abcbf
Tácio Mendes <tacio.mendes@gmail.com>
2020-08-21 17:10:17 -0300

    Project Created

---
+++ src/main/java/com/redvector/udemyspringBoot/UdemySpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.redvector.udemyspringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemySpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdemySpringBootApplication.class, args);
+	}
+
+}
