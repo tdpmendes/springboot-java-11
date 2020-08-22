fcb2dc0cc7a6c566ee6c84d44b6fa5a9a3a2b8d5
Tácio Mendes <tacio.mendes@gmail.com>
2020-08-21 21:26:55 -0300

    Subtotal and total methods

--- src/main/java/com/redvector/udemyspringBoot/entities/Order.java
+++ src/main/java/com/redvector/udemyspringBoot/entities/Order.java
@@ -86,6 +86,14 @@ public class Order implements Serializable{
 		if(orderStatus != null)	this.orderStatus = orderStatus.getCode();
 	}
 
+	public Double getTotal() {
+		double sum = 0;
+		for (OrderItem orderItem : items) {
+			sum += orderItem.getSubTotal();
+		}
+		return sum;
+	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
