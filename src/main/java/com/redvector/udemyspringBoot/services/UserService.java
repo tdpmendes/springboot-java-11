75e619bcc9cbb303fb3c4edfd9e984236fe59607
Tácio Mendes <tacio.mendes@gmail.com>
2020-08-21 22:03:45 -0300

    Exception Handling - FindById

--- src/main/java/com/redvector/udemyspringBoot/services/UserService.java
+++ src/main/java/com/redvector/udemyspringBoot/services/UserService.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.redvector.services.exceptions.ResourceNotFoundException;
 import com.redvector.udemyspringBoot.entities.User;
 import com.redvector.udemyspringBoot.repositories.UserRepository;
 
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> user = repository.findById(id);
-		return user.get();
+		return user.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
